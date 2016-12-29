b463e58fc8e7e48d94b6fa40ed4143d33bdd9ae7
svcastaneda <svcastaneda@gmail.com>
2016-12-28 19:44:12 -0600

    Add in a couple of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'products#index'
+  get 'shop' => 'products#index'
+  get 'shop/:id' => 'products#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
