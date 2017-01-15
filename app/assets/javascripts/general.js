6b38ca9d12d163bff9ddfb5ed9a924369a7e3a4c
svcastaneda <svcastaneda@gmail.com>
2017-01-15 01:29:58 -0600

    Add in shitbot help command

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -9,4 +9,19 @@ $(document).ready(function () {
 		$("#startMenu").toggle();
 		$("#startBtn").toggleClass("inset");
 	});
+	
+	$(".menu-item").on("click", function(e) {
+		$("#startMenu").hide();
+		$("#startBtn").removeClass("inset");
+	});
+	
+	var updateTime = function() {
+	  setTimeout(updateTime, 1000);
+	  var now = new Date();
+		var time = now.toLocaleTimeString("en-US", { hour: 'numeric', minute: 'numeric', hour12: true });
+
+	  $("#time").text(time);
+	}
+	
+	updateTime();
 });
