3bf5400178b5fdefecc0f4c01fb4bad9773bb99f
svcastaneda <svcastaneda@gmail.com>
2017-01-15 01:33:05 -0600

    Make windows closable and fix z-index

--- app/assets/javascripts/window.js
+++ app/assets/javascripts/window.js
@@ -1,15 +1,38 @@
 $(document).ready(function () {
+	var zIndex = 3;
+	
 	$(function() {
 		$('.window').draggable();
 	});
 	
+	$(".window").on("mousedown", function(e) {
+		// $(".window").css({"z-index": "2"});
+		$(this).css({"z-index": zIndex});
+		zIndex++;
+	});
+	
 	$(".glyph-min").on("click", function(e) {
 		$(this).closest(".window").addClass("minimized");
+		$(this).closest(".window").removeClass("maximized");
+		$(this).closest(".window").removeClass("closed");
 	});
 	
 	$(".glyph-max").on("click", function(e) {
+		$(this).closest(".window").addClass("maximized");
 	});
 	
 	$(".glyph-close").on("click", function(e) {
+		$(this).closest(".window").css('visibility', 'hidden');
+	});
+	
+	$(".icon").on("click", function(e) {
+		var n = $(this).attr("name");
+		zIndex = 3;
+		$(".window").css({"z-index": "2"});
+		$("#" + n).css({"visibility": "initial", "z-index": zIndex});
+	});
+	
+	$("#helpLauncher").on("click", function(e) {
+		$("#help").css({"display": "inline-block", "visibility": "initial", "z-index": zIndex});
 	});
 });
